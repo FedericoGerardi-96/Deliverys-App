5e661bb22637613fe8ac064731496d9394a1a7e0
Rodrigo Torres <54510225+Rodrigo00909@users.noreply.github.com>
2023-01-26 19:10:17 -0300

    navbar changes

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import Image from 'next/image';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NoblezaImg  from "../../public/assets/logo-sin-fondo.png";
 
 import {
   faCartShopping,
-  faCoffee,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
@@ -14,18 +15,25 @@ const Navbar = () => {
         {/* <h1 className='text-3xl font-bold underline hover:text-[#f3f3f3]'>Navbar</h1> */}
 
         <div className='flex'>
-            <div className="w-1/3 bg-blue-800">
-                Icon
+            <div className="w-1/3">
+                <Image
+                    src={NoblezaImg}
+                    alt="Nobleza Logo"
+                    style={{
+                    width: '100px',
+                    height: '100px',
+                    }}
+                />
             </div>
-            <div className="w-2/3 flex-nowrap bg-blue-400">
-                <div>menu HOLA</div>
-                <div>menu 2</div>
-                <div>menu 3</div>
+            <div className="w-2/3 flex flex-nowrap">
+                <div className="w-1/3">menu HOLA</div>
+                <div className="w-1/3">menu 2</div>
+                <div className="w-1/3">menu 3</div>
             </div>
-            <div className="w-3/3 bg-blue-600">
+            <div className="w-3/3">
                 <FontAwesomeIcon
                     icon={faCartShopping}
-                    style={{ fontSize: 100, color: "blue" }}
+                    style={{ fontSize: 50, color: "#E86A12" }}
                 />
             </div>
         </div>
